6028ab0b7574059ef07560005db93c6e42c2d355
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2024-08-22 19:49:41 -0300

    Criação dos testes unitários para as classes de Exception.

---
+++ src/test/java/br/com/ecommerce/marcel/philippe/exception/advice/UsuarioControllerAdviceTest.java
@@ -0,0 +1,41 @@
+package br.com.ecommerce.marcel.philippe.exception.advice;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+
+import br.com.ecommerce.marcel.philippe.dto.ErrorDTO;
+import br.com.ecommerce.marcel.philippe.exception.UsuarioNotFoundException;
+
+public class UsuarioControllerAdviceTest {
+	
+	@InjectMocks
+    private UsuarioControllerAdvice usuarioControllerAdvice;
+
+    @Mock
+    private UsuarioNotFoundException usuarioNotFoundException;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void deveRetornarOErroHandleUsuarioNotFound() {
+        when(usuarioNotFoundException.getMessage()).thenReturn("Usuário não encontrado.");
+
+        ErrorDTO errorDTO = usuarioControllerAdvice.handleUserNotFound(usuarioNotFoundException);
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), errorDTO.getStatus());
+        assertEquals("Usuário não encontrado.", errorDTO.getMessage());
+        assertEquals(new Date().getClass(), errorDTO.getTimestamp().getClass());
+    }
+}
